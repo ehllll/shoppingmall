c7ee019172fafa4e20c507d7a7bb019be600df03
ehllll <leeeunhye0510@naver.com>
2025-05-20 16:38:24 +0900

    feat: Board CRUD 완성

--- src/main/java/com/example/shoppingmall/domain/board/entity/Board.java
+++ src/main/java/com/example/shoppingmall/domain/board/entity/Board.java
@@ -1,12 +1,17 @@
 package com.example.shoppingmall.domain.board.entity;
 
+import com.example.shoppingmall.domain.board.dto.BoardUpdateRequestDto;
 import com.example.shoppingmall.global.common.entity.BaseEntity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -23,5 +28,23 @@ public class Board extends BaseEntity {
 	private String title;
 	private String content;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "store_id")
+	private Board board;
+
+
+	public Board(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
+
+	public void updatedAt(BoardUpdateRequestDto boardUpdateRequestDto) {
+		if(boardUpdateRequestDto.getTitle() != null) {
+			this.title = boardUpdateRequestDto.getTitle();
+		}
+		if(boardUpdateRequestDto.getContent() != null) {
+			this.content = boardUpdateRequestDto.getContent();
+		}
+	}
 }
 
--- src/main/java/com/example/shoppingmall/domain/board/service/BoardService.java
+++ src/main/java/com/example/shoppingmall/domain/board/service/BoardService.java
@@ -1,4 +1,22 @@
 package com.example.shoppingmall.domain.board.service;
 
+import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.shoppingmall.domain.board.dto.BoardRequestDto;
+import com.example.shoppingmall.domain.board.dto.BoardResponseDto;
+import com.example.shoppingmall.domain.board.dto.BoardUpdateRequestDto;
+
 public interface BoardService {
+	BoardResponseDto createPost(Long storeId, BoardRequestDto boardRequestDto);
+
+	BoardResponseDto findById(Long id);
+
+	List<BoardResponseDto> getAll();
+
+	@Transactional
+	void updatePost(Long storeId, Long id, BoardUpdateRequestDto boardUpdateRequestDto);
+
+	void deletePost(Long storeId, Long id, BoardRequestDto boardRequestDto);
 }
