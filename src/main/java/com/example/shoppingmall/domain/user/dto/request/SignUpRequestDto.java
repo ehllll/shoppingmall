5c893b3bbe7df13581c2814c8f89b31b8e0b763c
devgogogogo <imleo322@gmail.com>
2025-05-23 16:39:55 +0900

    fix: 수정1

--- src/main/java/com/example/shoppingmall/domain/user/dto/request/SignUpRequestDto.java
+++ src/main/java/com/example/shoppingmall/domain/user/dto/request/SignUpRequestDto.java
@@ -8,16 +8,14 @@ public class SignUpRequestDto {
 
     private final String nickname;
     private final String username;
-    private final String email;
     private final String password;
     private final String address;
     private final UserRole userAuthority;
 
 
-    public SignUpRequestDto(String nickname, String username, String email1, String password, String address, UserRole userAuthority) {
+    public SignUpRequestDto(String nickname, String username, String password, String address, UserRole userAuthority) {
         this.nickname = nickname;
         this.username = username;
-		this.email = email1;
 		this.password = password;
         this.address = address;
         this.userAuthority = userAuthority;
